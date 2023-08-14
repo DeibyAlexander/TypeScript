2656df2ebfbcaa3650affb2f4eaae3141ce61d99
DeibyAlexander <deiby2707@gmail..com>
2023-08-14 09:19:52 -0500

    feat: :sparkles: TypeScript Avances

---
+++ into-typescript/dist/tipos.js
@@ -0,0 +1,34 @@
+"use strict";
+let wonderFulGroup = "Hola";
+wonderFulGroup = "Artemis";
+console.log(wonderFulGroup);
+//! Tipos de datos de Javascript
+//* number
+let artemisQuantity = 40;
+console.log(typeof (artemisQuantity));
+//* string
+let artemisName = "V1";
+console.log(typeof artemisName);
+//* boolean
+let artemisHired = true;
+console.log(typeof (artemisHired));
+//* null
+//* undefined
+/* let artemisSygned
+console.log(artemisSygned); */
+//* object
+//* function
+//! Tipos de datos de Typescript (Añadidos)
+//* any -> Avoid Its Use (Evitar su uso)
+let artemisSygned;
+console.log(artemisSygned);
+let artemisAny = 10;
+console.log(artemisAny);
+//* unknown
+//* never
+//* arrays
+//* tuplas
+//* anums
+//! Tipos de datos Inferidos
+let artemisFired = false;
+console.log(typeof (artemisFired));
---
+++ into-typescript/src/tipos.ts
@@ -0,0 +1,66 @@
+
+
+let wonderFulGroup:string = "Hola";
+
+
+wonderFulGroup = "Artemis";
+console.log(wonderFulGroup);
+
+//! Tipos de datos de Javascript
+
+
+//* number
+
+let artemisQuantity:number = 40
+console.log(typeof(artemisQuantity));
+
+//* string
+
+let artemisName:string = "V1"
+console.log(typeof artemisName);
+
+//* boolean
+
+let artemisHired:boolean = true
+console.log(typeof(artemisHired));
+
+//* null
+
+//* undefined
+
+/* let artemisSygned
+console.log(artemisSygned); */
+
+//* object
+
+//* function
+
+
+//! Tipos de datos de Typescript (Añadidos)
+
+
+//* any -> Avoid Its Use (Evitar su uso)
+
+let artemisSygned
+console.log(artemisSygned);
+
+let artemisAny:any = 10
+artemisAny = "anything"
+console.log(artemisAny);
+
+//* unknown
+
+//* never
+
+//* arrays
+
+//* tuplas
+
+//* anums
+
+
+//! Tipos de datos Inferidos
+
+
+let artemisFired = false
+console.log(typeof(artemisFired));
